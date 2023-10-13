c95a1be4ae7c52c2894aea1bf31f8c095c1be861
Amlan Alok <amlanalok@gmail.com>
2023-10-13 18:27:37 -0400

    spring entry class file added

---
+++ src/main/java/com/client/Application.java
@@ -0,0 +1,13 @@
+package com.client;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  // entry point of spring boot application
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
