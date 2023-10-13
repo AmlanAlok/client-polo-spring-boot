af46f45fa76773f93477292f053e9e967fc770a5
Amlan Alok <amlanalok@gmail.com>
2023-10-13 18:32:31 -0400

    getters, setters and constructors added

--- src/main/java/com/client/entity/Students.java
+++ src/main/java/com/client/entity/Students.java
@@ -24,4 +24,54 @@ public class Students {
     @Column(name = "updated_at")
     private Timestamp updatedAt;
 
+    public Students() {
+    }
+
+    public Students(Long id, String firstName, String lastName, Timestamp createdAt, Timestamp updatedAt) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
