7f8cecd78c36139c6f995972ad37b2c1a58bd56d
Amlan Alok <amlanalok@gmail.com>
2023-10-15 13:43:23 -0400

    get student by ID API added

---
+++ src/main/java/com/client/controller/StudentController.java
@@ -0,0 +1,36 @@
+package com.client.controller;
+
+import com.client.entity.Student;
+import com.client.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/student")
+public class StudentController {
+
+    private static final Logger logger = Logger.getLogger(StudentController.class.getName());
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/getAll")
+    private List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+    @PostMapping("/save")
+    private Student saveStudent(@RequestBody Student student){
+        return studentService.saveStudent(student);
+    }
+
+    @GetMapping("/getById/{id}")
+    private Student getStudentById(@PathVariable Long id){
+        return studentService.getStudentById(id);
+    }
+
+}
---
+++ src/main/java/com/client/service/StudentService.java
@@ -0,0 +1,60 @@
+package com.client.service;
+
+import com.client.entity.Student;
+import com.client.exception.StudentNotFoundException;
+import com.client.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Service
+public class StudentService {
+
+    public static final Logger logger = Logger.getLogger(StudentService.class.getName());
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<Student> getAllStudents(){
+
+        try {
+            List<Student> studentList = studentRepository.findAll();
+            logger.info("Successfully retrieved -> "+ studentList.size()+" students info");
+            return studentList;
+        }
+        catch(Exception e){
+            logger.log(Level.SEVERE, "Exception occurred while fetching all students from the RDS", e);
+            throw e;
+        }
+    }
+
+    public Student saveStudent(Student student){
+
+        try {
+            student.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+            Student savedStudent = studentRepository.save(student);
+            return savedStudent;
+        }
+        catch (Exception e){
+            logger.log(Level.SEVERE, "Exception occurred while saving student from the RDS");
+            throw e;
+        }
+    }
+
+    public Student getStudentById(Long id){
+        try {
+            return studentRepository.findById(id)
+                    .orElseThrow(() -> new StudentNotFoundException(id));
+        }
+        catch (Exception e){
+            logger.log(Level.SEVERE, "Exception occurred while getting student using ID");
+            throw e;
+        }
+    }
+
+
+}
